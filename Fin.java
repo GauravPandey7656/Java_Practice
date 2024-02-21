227b598a2e59b4a70e3107ec5338d3235f6a58eb
Gaurav Pandey <114379619+GauravPandey7656@users.noreply.github.com>
2024-02-21 19:49:32 +0530

    Add files via upload

---
+++ Fin.java
@@ -0,0 +1,8 @@
+class Fin {
+    public static void main(String[] args){
+        final int A=10;
+        System.out.println(A);
+        final int B=20;
+        System.out.println(B);
+    }    
+}
